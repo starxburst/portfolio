638e3b6fbbafd37939bec529968974c93cef03e7
starxburst <starburstxstream@gmail.com>
2022-08-03 02:48:37 +0800

    deleted windows Img

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import '../styles/About.css'
-import windowImg from '../assets/window.png'
 import Typewriter from 'typewriter-effect';
 
 const About = () => {
@@ -11,10 +10,12 @@ const About = () => {
         const target = document.getElementById("aboutContainer");
         const windowHeight = window.innerHeight;
         const elementTop = target.getBoundingClientRect().top;
-        if (elementTop - windowHeight <= 0) {
+        if (elementTop - windowHeight < 0) {
             setElementVisible(true);
             console.log(elementVisible);
-        } 
+        } else {
+            setElementVisible(false);
+        }
         console.log(elementTop);
         console.log(window.innerHeight);
     }
@@ -25,31 +26,13 @@ const About = () => {
 
     return (
         <div id="aboutContainer" className="about-container">
-            <div className="about-header">
-                <h1> About Me </h1>
-                
-            </div>
             <div className="about-content-container">
-                <div className="window-container">       
-                    <img className="window-image" src={windowImg} alt="window" />     
-                    {elementVisible? (
-                        <div className="about-typewriter-container">
-                            <Typewriter
-                            options={{
-                                strings: ['> Welcome to my Portfolio. I graduated from Hong Kong Baptist University majoring in green energy science. I started my programming journey when I was 12 years old. I was obsessed with video games and put many times into them. During that time, I touched the programming step by step. I could contribute to the community, and exchange ideas with other developers. Currently, I’m trying to become a full-stack Javascript developer. Web development allows me to demonstrate creativity, create eye-catching animation and styling in numerous ways, turning designs into high-quality code. After over a year of study, I am ready to contribute to the world in this new way.'],
-                                autoStart: true,
-                                loop: true,
-                                delay: 20,
-                                pauseFor: 10000000,
-                                wrapperClassName: 'about-typewriter-wrapper',
-                                cursorClassName: 'typewriter-cursor'
-                            }}
-                            />
-                        </div>
-                    ):
-                    null}
-                </div>
-            </div>
+                <h1 className="about-header"> Hi. I'm Eric. Welcome to my portfolio </h1>
+                {elementVisible? (
+                    <h2 className="about-content-description">Many people asked me Why did you try to step into a field that is unrelated to my college major? And the answer is quite simple. Programming gives me satisfaction. As a self-taught web developer, I always keep pushing myself to explore any new technology. The constant change and the opportunity to learn new aspects are really important for me. I have always found a certain satisfaction in programming and I knew I wanted to make a career in it.</h2>
+                ):
+                null}
+    </div>
         </div>
     )
 }
